fb5a07cbe4650f06adc76531c76458c32ca25460
Ihor110986 <ihor110986@gmail.com>
2022-02-15 12:39:33 +0200

    Create todo list at php

---
+++ src/configDB.php
@@ -0,0 +1,4 @@
+<?php
+    $dsn = 'mysql:host=new.localhost;dbname=to-do';
+    $pdo = new PDO($dsn, 'root', 'aabbcc1109');
+?>
---
+++ src/index.php
@@ -0,0 +1,32 @@
+!<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-do list</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>To-do list</h1>
+        <form action="/add.php" method="post">
+            <input type="text" name="task" id="task" placeholder="Must  be to make.." class="form-control">
+            <button type="submit" name="sendTask" class="btn btn-cuccess">Send</button>
+        </form>
+
+        <?php
+         require 'configDB.php';
+
+         echo '<ul>';
+         $query =$pdo->query('SELECT * FROM `tasks` ORDER BY `id` DESC');
+         while($row = $query->fetch(PDO::FETCH_OBJ)) {
+             echo '<li><b>'.$row->task.'</b><a href="/delete.php?id='.$row->id.'"><button>Delete</button></a></li>';
+         }
+         echo '</ul>';
+        ?>
+    </div>
+
+</body>
+</html>
