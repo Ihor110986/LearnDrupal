8ef1e98bbb8f6b529d1cd6429c3d6686d62fb890
Ihor110986 <ihor110986@gmail.com>
2022-02-18 15:49:24 +0200

    Update auth.php

--- src/auth.php
+++ src/auth.php
@@ -11,20 +11,13 @@
     $query = "SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'";
     $result = $pdo->query($query);
     $user = $result->fetch(PDO::FETCH_ASSOC);
-    if(count($user) == 0) {
+    if ($user === false)  {
+
         echo "No such user found";
         exit();
     }
 
     setcookie('user', $user['name'], time() + 3600, "/");
-
-    // print_r($user);
-    // exit();
-
-
-
-    // $query = "INSERT INTO `users` (`email`, `login`, `pass`, `name`) VALUES ('$email', '$login', '$pass', '$name')";
-    // $result = $pdo->query($query);
 
     header('Location: /');
     exit();
